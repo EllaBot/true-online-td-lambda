d5395fc4906c3fafd0580edfbc6215f24fc66b03
Rishi Shah <rishihahs@gmail.com>
2015-04-30 19:25:20 -0500

    Cartesian product

--- tests/test_fourierbasis.py
+++ tests/test_fourierbasis.py
@@ -7,15 +7,20 @@ class TestFourierBasis(object):
 
     def test_get_num_basis_functions(self):
         ranges = None
-        n = 2
-        d = 3
-        fourierbasis = FourierBasis(ranges, n, d)
+        d = 2
+        n = 3
+        fourierbasis = FourierBasis(ranges, d, n)
         assert_equal(fourierbasis.get_num_basis_functions(), 16.0)
 
-    #def test_multipliers(self):
-    #    n = 2
-    #    d = 3
-    #    multipliers = fourierbasis.__multipliers(n, d).tolist()
-    #    assert_list_equal(multipliers, [0, 10, 1, 11])
+    def test_cartesian(self):
+        arrays = ([0, 1, 2], [0, 1, 2])
+        result = FourierBasis.cartesian(arrays).tolist()
+        assert_list_equal(result, [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]])
+
+    def test_multipliers(self):
+        d = 2
+        n = 3
+        multipliers = fourierbasis.__multipliers(d, n).tolist()
+        assert_list_equal(multipliers, [0, 10, 1, 11])
 
 
--- true_online_td_lambda/fourierbasis.py
+++ true_online_td_lambda/fourierbasis.py
@@ -30,3 +30,58 @@ class FourierBasis(object):
 
         self.num_functions = (self.n + 1.0) ** self.d
         return self.num_functions
+
+    def __multipliers(self, d, n):
+        """Generates multipliers for the fourier basis.
+        This corresponds to the c vector in the paper
+        """
+        iter = itertools.product(range(n + 1), repeat = d)
+        return numpy.array([list(map(int, x)) for x in iter])
+
+    @staticmethod
+    def cartesian(arrays, out=None):
+        """Generate a cartesian product of input arrays.
+        Parameters
+        ----------
+        arrays : list of array-like
+            1-D arrays to form the cartesian product of.
+        out : ndarray
+            Array to place the cartesian product in.
+        Returns
+        -------
+        out : ndarray
+            2-D array of shape (M, len(arrays)) containing cartesian products
+            formed of input arrays.
+        Examples
+        --------
+        >>> cartesian(([1, 2, 3], [4, 5], [6, 7]))
+        array([[1, 4, 6],
+               [1, 4, 7],
+               [1, 5, 6],
+               [1, 5, 7],
+               [2, 4, 6],
+               [2, 4, 7],
+               [2, 5, 6],
+               [2, 5, 7],
+               [3, 4, 6],
+               [3, 4, 7],
+               [3, 5, 6],
+               [3, 5, 7]])
+
+        From scikit-learn's extra math
+        """
+        arrays = [numpy.asarray(x) for x in arrays]
+        shape = (len(x) for x in arrays)
+        dtype = arrays[0].dtype
+
+        ix = numpy.indices(shape)
+        ix = ix.reshape(len(arrays), -1).T
+
+        if out is None:
+            out = numpy.empty_like(ix, dtype=dtype)
+
+        for n, arr in enumerate(arrays):
+            out[:, n] = arrays[n][ix[:, n]]
+
+        return out
+
