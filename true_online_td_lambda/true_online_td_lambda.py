e779d213a9225520e3fd52781ad0d549df426180
Nick Walker <nick@nickwalker.us>
2015-05-03 00:32:39 -0500

    Correct trace update math

--- true_online_td_lambda/true_online_td_lambda.py
+++ true_online_td_lambda/true_online_td_lambda.py
@@ -1,13 +1,14 @@
 from basis import FourierBasis
 import numpy as np
 from random import random
+
+
 class TrueOnlineTDLambda(object):
     def __init__(self, numfeatures, ranges):
         self.alpha = 0.01
-        self.epsilon = 0.1
         self.lmbda = 0.7
         self.gamma = 1.0
-        self.basis = FourierBasis(ranges, numfeatures)
+        self.basis = FourierBasis(ranges, numfeatures, 3)
         self.stateprime = None
         self.state = None
         self.theta = np.zeros(self.basis.get_num_basis_functions())
@@ -30,9 +31,12 @@ class TrueOnlineTDLambda(object):
 
         # See "True Online TD(lambda)" section 4.1
         delta = reward + (self.gamma * vsprime) - self.vs
-        termone = self.gamma * self.lmbda * self.traces + self.alpha
-        termtwo = 1 - self.gamma * self.lmbda * np.dot(self.traces, phi_t) * phi_t
-        self.traces = termone * termtwo
+        termone = self.gamma * self.lmbda * self.traces
+        termtwo = self.alpha * (1 - self.gamma * self.lmbda * np.dot(self.traces, phi_t)) * phi_t
+        self.traces = termone + termtwo
         self.theta += delta * self.traces + self.alpha * (vsprime - np.dot(self.theta, phi_t)) * phi_t
 
         self.vs = vsprime
+
+    def value(self, state):
+        return np.dot(self.theta, self.basis.compute_features(state))
