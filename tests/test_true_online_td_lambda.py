fe27d5caae807ddb52b1a5e1ed1310afab62360b
Rishi Shah <rishihahs@gmail.com>
2015-05-04 11:36:46 -0500

    Test for maximize_value

--- tests/test_true_online_td_lambda.py
+++ tests/test_true_online_td_lambda.py
@@ -1,4 +1,5 @@
 from true_online_td_lambda import TrueOnlineTDLambda
+import math
 from nose.tools import assert_greater, assert_equal, assert_almost_equal
 from numpy.testing import assert_array_equal
 
@@ -43,3 +44,19 @@ class TestTrueOnlineTDLambda(object):
         assert_array_equal(learner.theta, learner_scaled.theta)
         assert_almost_equal(learner.value([1.0, 1.0]), learner_scaled.value([20.0, 20.0]))
         assert_almost_equal(learner.value(state_one), learner_scaled.value(state_one))
+
+    def test_maximize_value(self):
+        learner = TrueOnlineTDLambda(2, [(0, 10), (0, 10)])
+
+        for n in range(100):
+            i = n / 10.0
+            diff = i - 7.6 # 7.6 is the target
+            learner.step(-math.sqrt(diff ** 2 + diff ** 2), [float(i), float(i)])
+
+        state_1 = 5.0 # fix state one
+        max_action = learner.maximize_value([state_1])
+
+        print(max_action)
+
+        assert_greater(learner.value([state_1, max_action]), learner.value([state_1, max_action - 0.01]))
+        assert_greater(learner.value([state_1, max_action]), learner.value([state_1, max_action + 0.01]))
