11d39837a97ff50e8835c9ea495b3db43c2a4508
Rishi Shah <rishihahs@gmail.com>
2015-05-08 18:29:21 -0500

    Make interface for l_bfgs more consistent

--- true_online_td_lambda/optimization/l_bfgs.py
+++ true_online_td_lambda/optimization/l_bfgs.py
@@ -1,8 +1,14 @@
 import numpy, scipy.optimize
 
-def maximize(func, x0, func_prime, bounds=None):
+def maximize(func, x0=None, func_prime=None, bounds=None):
     # Multiply by -1 since we want to maximize
     f = lambda x: -func(x)
+
+    if x0 is None:
+        raise ValueError("x0 must be provided")
+    if func_prime is None:
+        raise ValueError("func_prime must be provided")
+
     # Ravel func_prime
     fprime = lambda x: -func_prime(x)
     return scipy.optimize.fmin_l_bfgs_b(f, x0, fprime, bounds=bounds)[0]
