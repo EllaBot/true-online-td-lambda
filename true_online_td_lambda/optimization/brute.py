f38408d4f6b71b78b0ed3eea51e5ae06b831fecc
Rishi Shah <rishihahs@gmail.com>
2015-05-08 18:28:24 -0500

    Provide return values in specific format for scipy.optimize.brute

--- true_online_td_lambda/optimization/brute.py
+++ true_online_td_lambda/optimization/brute.py
@@ -18,8 +18,11 @@ def maximize(func, x0=None, func_prime=None, bounds=None):
         kwargs['bounds'] = bounds
         if fprime is None:
             kwargs['approx_grad'] = 1
-        return scipy.optimize.fmin_l_bfgs_b(*args, **kwargs)
 
-    # Ranges: step by 0.01
+        output = scipy.optimize.fmin_l_bfgs_b(*args, **kwargs)
+
+        return (output[0], output[1], output[2]['warnflag'])
+
+    # Ranges: step size is default
     return scipy.optimize.brute(f, tuple(bounds),
-                                   finish=__l_bfgs_b)[0]
+                                   finish=__l_bfgs_b)
